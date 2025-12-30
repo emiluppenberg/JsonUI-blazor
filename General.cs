f49513e8a23011f70be99e653d3a8a4d0e3213a2
twofourZ <uppenberg95@gmail.com>
2025-12-30 11:11:32 +0100

    Enabled sticky buttons for objects/arrays and with scroll-into-view functionality.

---
+++ General.cs
@@ -0,0 +1,5 @@
+public static class General
+{
+  public const int RowHeightPx = 22;
+  public const int ColWidthPx = 160;
+}
---
+++ wwwroot/js/helpers.js
@@ -0,0 +1,27 @@
+window.createStickyObserver = (sentinel, dotNetRef) => {
+  const observer = new IntersectionObserver(
+    ([entry]) => {
+      dotNetRef.invokeMethodAsync(
+        "OnStickyStateChanged",
+        !entry.isIntersecting
+      );
+    },
+    { threshold: 1 }
+  );
+
+  observer.observe(sentinel);
+
+  return {
+    disconnect: () => observer.disconnect(),
+  };
+};
+
+window.scrollToElement = (element) => {
+  const container = document.getElementById("node-container");
+  const cRect = container.getBoundingClientRect();
+  const eRect = element.getBoundingClientRect();
+
+  const scrollTo = eRect.top - cRect.top + container.scrollTop + 1;
+
+  container.scrollTo({ top: scrollTo, behavior: "smooth" });
+};
